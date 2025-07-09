ffafe1d05c1d2f7e685d4e78101855ac681ec8a5
AHOVEY Saint-George <sgahovey@gmail.com>
2025-07-09 08:40:20 +0400

    Connexion à la base de données MySQL via bdd.php

---
+++ lib/bdd.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'gestion_taches';
+$user = 'root';
+$password = ''; // par défaut avec WAMP
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
