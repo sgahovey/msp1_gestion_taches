a101be868ff98e6b24b4500f127b88fd9c7b1298
AHOVEY Saint-George <sgahovey@gmail.com>
2025-07-09 08:49:08 +0400

    Ajout de ui.php avec Bootstrap 5, Fancybox et css

---
+++ lib/ui.php
@@ -0,0 +1,10 @@
+<!-- Bootstrap 5 -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<!-- Fancybox -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css" />
+<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script>
+
+<!-- Ton style perso -->
+<link rel="stylesheet" href="css/style.css">
