084735724619670a11975b51a8eb418f39ff41d5
AHOVEY Saint-George <sgahovey@gmail.com>
2025-07-09 11:31:10 +0400

    Suppression du fonction non utilisée

--- lib/fonctions.php
+++ lib/fonctions.php
@@ -14,21 +14,3 @@
         echo '</div>';
     }       
 
-  function processAddTache(PDO $pdo): ?string {
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $titre = $_POST['titre'] ?? '';
-        $description = $_POST['description'] ?? '';
-        $priorite = $_POST['priorite'] ?? 'Normale';
-        $date_limite = $_POST['date_limite'] ?? null;
-
-        if (addTache($pdo, $titre, $description, $priorite, $date_limite, 0)) {
-            echo "<script>window.parent.location.reload();</script>";
-            exit;
-        } else {
-            return "❌ Erreur lors de l'ajout.";
-        }
-    }
-
-    return null;
-}
-
